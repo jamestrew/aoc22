7dcebfc2b68b28340ba60452eaf20c55d8edd194
James Trew <j.trew10@gmail.com>
2022-12-09 19:54:26 -0500

    hashmap

--- utils/structs.go
+++ utils/structs.go
@@ -25,3 +25,34 @@ func (s *Set[T]) Del(val T) {
 func (s *Set[T]) Length() int {
 	return len(s.vals)
 }
+
+type HashMap[K comparable, V any] struct {
+	_map map[K]V
+}
+
+func NewHashMap[K comparable, V any]() *HashMap[K, V] {
+	return &HashMap[K, V]{_map: make(map[K]V)}
+}
+
+func (h *HashMap[K, V]) Set(key K, val V) {
+	_, ok := h._map[key]
+	if !ok {
+		h._map[key] = val
+	}
+}
+
+func (h *HashMap[K, V]) Get(key K, _default V) V {
+	val, ok := h._map[key]
+	if !ok {
+		return _default
+	}
+	return val
+}
+
+func (h *HashMap[K, V]) Del(key K) V {
+	val, ok := h._map[key]
+	if ok {
+		delete(h._map, key)
+	}
+	return val
+}
