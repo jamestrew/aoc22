61c53a53c99b2dedae6dcb70cfcc772b3442d9cd
James Trew <j.trew10@gmail.com>
2022-12-09 19:59:35 -0500

    expand fp a bit

--- utils/fp.go
+++ utils/fp.go
@@ -1,10 +1,32 @@
 package utils
 
-func Map[T any, S any](fn func(T) S, s []T) []S {
-	ret := make([]S, len(s))
+import "strconv"
 
-	for _, v := range s {
+func Map[T any, S any](fn func(T) S, slice []T) []S {
+	ret := make([]S, 0, len(slice))
+
+	for _, v := range slice {
 		ret = append(ret, fn(v))
+	}
+	return ret
+}
+
+func MapStrInt(slice []string) []int {
+	fn := func(x string) int {
+		i, _ := strconv.Atoi(x)
+		return i
+	}
+
+	return Map(fn, slice)
+}
+
+func Filter[T any](fn func(T) bool, slice []T) []T {
+	ret := make([]T, 0, len(slice))
+
+	for _, v := range slice {
+		if fn(v) {
+			ret = append(ret, v)
+		}
 	}
 	return ret
 }
