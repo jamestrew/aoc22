22b730a39bde359a2bef8a348e6e1700739a4d86
James Trew <j.trew10@gmail.com>
2022-12-13 19:40:54 -0500

    day0

---
+++ day0/day0.go
@@ -0,0 +1,22 @@
+package day0
+
+import (
+	"fmt"
+
+	"github.com/jamestrew/aoc22/utils"
+)
+
+func part1(input string) int {
+	ret := 0
+	return ret
+}
+
+func part2(input string) int {
+	ret := 0
+	return ret
+}
+
+func Answers() {
+	fmt.Println(part1(utils.GetInput(0)))
+	fmt.Println(part2(utils.GetInput(0)))
+}
---
+++ day0/day0_test.go
@@ -0,0 +1,17 @@
+package day0
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const input = ``
+
+func TestPart1Example(t *testing.T) {
+	assert.Equal(t, 0, part1(input))
+}
+
+func TestPart2Example(t *testing.T) {
+	assert.Equal(t, 0, part2(input))
+}
