c5b03e6f0bacb4bac89c9163f33424ff00cc9c32
James Trew <j.trew10@gmail.com>
2023-07-04 20:28:26 -0400

    rust: day5 part 1 take 1?

---
+++ day5/src/main.rs
@@ -0,0 +1,40 @@
+use aoc22::day_inputs;
+use day5::take1::*;
+
+fn main() {
+    let input = day_inputs(5);
+    println!("Part 1: {}", part1(&input));
+    // println!("Part 2: {}", part2(&input));
+}
+
+fn part1(input: &str) -> String {
+    let (stacks, instructions) = split_input(input);
+    let mut stacks = CargoStacks::from(stacks);
+    let instructions = Instructions::from(instructions);
+
+    stacks.apply_move_instructions(&instructions);
+    stacks.tops()
+}
+
+fn part2(input: &str) -> String {
+    todo!()
+}
+
+// #[cfg(test)]
+// mod test {
+//     const SAMPLE: &str = "\
+//     [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3
+//
+// move 1 from 2 to 1
+// move 3 from 1 to 3
+// move 2 from 2 to 1
+// move 1 from 1 to 2";
+//
+//     #[test]
+//     fn part1() {
+//         assert_eq!(crate::part1(SAMPLE), "CMZ");
+//     }
+// }
---
+++ day5/src/take1.rs
@@ -0,0 +1,211 @@
+use itertools::Itertools;
+use std::ops::Deref;
+
+pub fn split_input(input: &str) -> (&str, &str) {
+    let mut split = input.split("\n\n");
+    (split.next().unwrap(), split.next().unwrap())
+}
+
+#[derive(Debug, PartialEq)]
+pub struct CargoStacks(Vec<Vec<char>>);
+
+impl CargoStacks {
+    fn stack_count(value: &str) -> Option<usize> {
+        value
+            .lines()
+            .rev()
+            .next()
+            .and_then(|line| line.split_whitespace().last())
+            .and_then(|num_str| num_str.parse::<usize>().ok())
+    }
+
+    pub fn apply_move_instructions(&mut self, instructions: &Instructions) {
+        for instr in instructions.iter() {
+            for _ in 0..instr.count {
+                let cargo = self.0[instr.from - 1].pop().unwrap();
+                self.0[instr.to - 1].push(cargo);
+            }
+        }
+    }
+
+    pub fn tops(&self) -> String {
+        let mut ret = String::with_capacity(self.0.len());
+        for stack in self.0.iter() {
+            if let Some(top) = stack.last() {
+                ret.push(*top);
+            }
+        }
+        return ret;
+    }
+}
+
+impl Deref for CargoStacks {
+    type Target = Vec<Vec<char>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<&str> for CargoStacks {
+    fn from(value: &str) -> Self {
+        let stack_count = CargoStacks::stack_count(value).unwrap();
+        let mut stacks: Vec<Vec<char>> = vec![Vec::new(); stack_count];
+
+        value.lines().rev().skip(1).for_each(|line| {
+            line.chars()
+                .chunks(4)
+                .into_iter()
+                .enumerate()
+                .map(|(idx, chunk)| (idx, chunk.collect::<Vec<_>>()))
+                .filter(|(_, chunk)| chunk[1].is_alphabetic())
+                .for_each(|(idx, chunk)| stacks[idx].push(chunk[1]))
+        });
+
+        Self(stacks)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    pub count: usize,
+    pub from: usize,
+    pub to: usize,
+}
+
+#[derive(Debug)]
+pub struct Instructions(Vec<Instruction>);
+
+impl From<&str> for Instructions {
+    fn from(value: &str) -> Self {
+        let mut instructions = Vec::new();
+        value.lines().for_each(|line| {
+            let parts = line
+                .split_whitespace()
+                .filter_map(|s| s.parse::<usize>().ok())
+                .collect::<Vec<_>>();
+
+            instructions.push(Instruction {
+                count: parts[0],
+                from: parts[1],
+                to: parts[2],
+            })
+        });
+
+        Self(instructions)
+    }
+}
+
+impl Deref for Instructions {
+    type Target = Vec<Instruction>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    const INPUT: &str = "
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+    const CARGO: &str = "
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3";
+
+    const INSTRUCTIONS: &str = "\
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+    use super::*;
+
+    #[test]
+    fn test_split_input() {
+        let (stack, instructions) = split_input(INPUT);
+        assert_eq!(stack, CARGO);
+        assert_eq!(instructions, INSTRUCTIONS);
+    }
+
+    #[test]
+    fn stack_count() {
+        assert_eq!(CargoStacks::stack_count(CARGO), Some(3));
+    }
+
+    #[test]
+    fn parse_cargo() {
+        assert_eq!(
+            *CargoStacks::from(CARGO),
+            vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P'],]
+        );
+    }
+
+    #[test]
+    fn parse_instructions() {
+        assert_eq!(
+            Instructions::from(INSTRUCTIONS).0,
+            vec![
+                Instruction {
+                    count: 1,
+                    from: 2,
+                    to: 1
+                },
+                Instruction {
+                    count: 3,
+                    from: 1,
+                    to: 3
+                },
+                Instruction {
+                    count: 2,
+                    from: 2,
+                    to: 1
+                },
+                Instruction {
+                    count: 1,
+                    from: 1,
+                    to: 2
+                },
+            ],
+        );
+    }
+
+    #[test]
+    fn first_instruction() {
+        let mut stacks = CargoStacks(vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']]);
+        let instructions = Instructions(vec![Instruction {
+            count: 1,
+            from: 2,
+            to: 1,
+        }]);
+
+        stacks.apply_move_instructions(&instructions);
+
+        assert_eq!(
+            *stacks,
+            vec![vec!['Z', 'N', 'D'], vec!['M', 'C'], vec!['P']]
+        );
+    }
+
+    #[test]
+    fn whole_sample() {
+        let mut stacks = CargoStacks::from(CARGO);
+        let instructions = Instructions::from(INSTRUCTIONS);
+        stacks.apply_move_instructions(&instructions);
+
+        assert_eq!(
+            *stacks,
+            vec![vec!['C'], vec!['M'], vec!['P', 'D', 'N', 'Z']]
+        );
+    }
+}
